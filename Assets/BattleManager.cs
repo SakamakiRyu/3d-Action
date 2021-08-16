91571bfe79bd8429d284658fb2d9459a4652b4ea
SakamakiRyu <potesara031w@gmail.com>
2021-08-16 11:22:39 +0900

    StatusBaseクラスの作成をした

---
+++ Assets/BattleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    static BattleManager m_instance;
+    public static BattleManager Instance => m_instance;
+    private BattleManager() { }
+
+    private void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+        }
+    }
+
+    /// <summary>
+    /// ダメージ計算をし、ダメージ結果を攻撃対象に渡す
+    /// </summary>
+    /// <param name="targetStatus">攻撃対象のステータス</param>
+    /// <param name="attacker">攻撃者のステータス</param>
+    /// <returns>計算されたダメージ量</returns>
+    public int DamageCalculator(StatusBase targetStatus, StatusBase attacker)
+    {
+        int totalDamage = 0;
+        totalDamage += attacker.SendMyAttackPower();
+        return totalDamage;
+    }
+}
---
+++ Assets/StatusBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatusBase : MonoBehaviour
+{
+    [Header("ステータス")]
+    /// <summary>ヒットポイント</summary>
+    [SerializeField] protected int m_hp = default;
+    /// <summary>攻撃力</summary>
+    [SerializeField] protected int m_atkPower = default;
+
+    /// <summary>
+    /// 攻撃メソッド
+    /// </summary>
+    /// <param name="target"></param>
+    public virtual void Attack(StatusBase target)
+    {
+        Debug.Log("攻撃");
+        BattleManager.Instance.DamageCalculator(target, this);
+    }
+
+    /// <summary>自身の攻撃力を渡す</summary>
+    public virtual int SendMyAttackPower()
+    {
+        int totalAtkPower = default;
+        totalAtkPower += m_atkPower;
+        return totalAtkPower;
+    }
+
+    /// <summary>ダメージを受けた際の処理</summary>
+    /// <param name="damage"></param>
+    public virtual void Damaged(int damage)
+    {
+        Debug.Log("ダメージを受ける");
+    }
+}
