4272ed361ee05f569000de219d1357e509eb5b73
SakamakiRyu <potesara031w@gmail.com>
2021-08-21 23:11:25 +0900

    作業の更新をした

---
+++ Assets/Scripts/AttributeType.cs
@@ -0,0 +1,14 @@
+﻿public enum AttributeType
+{
+    /// <summary>無属性</summary>
+    None, 
+
+    /// <summary>火</summary>
+    Fire,
+
+    /// <summary>水</summary>
+    Water,
+
+    /// <summary>自然</summary>
+    Nature
+}
