d99c9d2738a97aa68a12bf076539ecbd60974e16
SakamakiRyu <potesara031w@gmail.com>
2021-09-03 23:57:24 +0900

    作業の更新をした

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -1,6 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
+    public GameState m_state { get; set; } = GameState.Title;
+
+    public enum GameState
+    {
+        Title,
+        Game,
+    }
+
+    private void Update()
+    {
+        if (m_state == GameState.Title)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
    
 }
